3f7ad742920e3babd3e27813d739f3f48d13a265
Juliette Chevalier <juliettech.1393@gmail.com>
2018-11-26 09:52:11 +0100

    added livicons & codepen title try. also added some css features

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,3 +1,10 @@
+
+
+// import * from '../../LivIconsEvo/js/LivIconsEvo.Tools.js';
+// import * from '../../LivIconsEvo/js/LivIconsEvo.defaults.js';
+// import * from '../../LivIconsEvo/js/LivIconsEvo.min.js';
+
+
 //= require jquery
 //= require bootstrap
 //= require_tree .
